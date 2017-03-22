3dd9292fa73103386df51a527fb0252cb00bcaa8
kraser <kravalsergey@yandex.ru>
2017-03-22 21:05:01 +0700

    Generating Code with Gii

---
+++ engine/views/user.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Users */
+/* @var $form ActiveForm */
+?>
+<div class="user">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    
+        <div class="form-group">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        </div>
+    <?php ActiveForm::end(); ?>
+
+</div><!-- user -->
