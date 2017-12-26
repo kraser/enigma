bcecaa7beb9df83cca0095a5dd7d41c9bd35a94b
kraser <s.kraval@e2e4online.ru>
2017-12-26 15:59:19 +0700

    Widgets / Creating Widgets

--- engine/views/site/rape.php
+++ engine/views/site/rape.php
@@ -1,7 +1,10 @@
 <?php
 use yii\helpers\Html;
+use app\widgets\HelloWidget;
 ?>
 <i><b>RAPE RAPE</b>
 <?= Html::encode($name) ?>
 <br>
-<?= Html::encode($message) ?></i>
+<?= Html::encode($message) ?></i><br>
+<?= HelloWidget::widget(['message' => 'Rape me in the morning']) ?>
+<br>
