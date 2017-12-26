1c7fcc0ab5016404e56dbd96e743648f757176f4
kraser <s.kraval@e2e4online.ru>
2017-12-26 14:41:20 +0700

    Widgets / Using Widgets

--- engine/views/site/say.php
+++ engine/views/site/say.php
@@ -1,6 +1,13 @@
 <?php
 use yii\helpers\Html;
+use yii\jui\DatePicker;
 ?>
+<?= DatePicker::widget([
+    'model' => '',
+    'attribute' => 'from_date',
+    'language' => 'ru',
+    'dateFormat' => 'php:Y-m-d',
+]) ?>
 <?= Html::encode($output) ?>
 <br>
 <?= Html::encode($message) ?>
